b5ffaae6e0e78ff191782c1c48af093c863150eb
Shubham Singh <shubhamsingh@kalyankumar.idc.payugur.com>
2015-05-26 15:37:00 +0530

    Initial Commit

---
+++ getImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  getImages
+//
+//  Created by Shubham Singh on 26/05/15.
+//  Copyright (c) 2015 Shubham Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ getImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  getImages
+//
+//  Created by Shubham Singh on 26/05/15.
+//  Copyright (c) 2015 Shubham Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
